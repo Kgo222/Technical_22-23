d36eceb0a50f56574f265db7f0fac49734f958c4
Kaitlin Gowens <gokaitgo100@gmail.com>
2022-11-05 22:54:32 -0500

    Prep for first coding day

--- arm_control_flutter/lib/main.dart
+++ arm_control_flutter/lib/main.dart
@@ -1,26 +1,24 @@
 import 'package:flutter/material.dart';
 import 'theme.dart';
 import 'constants.dart';
+import 'homepage.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const myApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class myApp extends StatelessWidget {
+  const myApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // The app title is defined in constants.dart
+      // The app theme is defined in constants.dart
       title: Constants.appName,
       // The app theme is defined in theme.dart
       theme: appTheme,
       // This directs the app to start at the screen class called MyHomePage.
       // Currently the main screen is the default from Flutter, but we will
       // edit the screens and screen routes as needed.
-        home: const MyHomePage(title: Constants.appName),
-      );
+      home: const HomePage(title: Constants.appName),
+    );
   }
 }
