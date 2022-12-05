7c340be6a96b9abb7c992bcdeb06612f5391b207
Kaitlin Gowens <gokaitgo100@gmail.com>
2022-12-05 13:35:37 -0600

    Fix slider layout

--- arm_control_flutter/lib/constants.dart
+++ arm_control_flutter/lib/constants.dart
@@ -1,4 +1,4 @@
 // This is a place to put application-wide constants for easier maintenance.
 class Constants {
-  static const String appName = "SNDY Robot";
+  static const String appName = "S.U.N.D.A.Y";
 }
