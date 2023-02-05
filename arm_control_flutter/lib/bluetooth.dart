fc09a21acfc1d6da5ebd0b4db18f7db558eb6e1a
Kaitlin Gowens <gokaitgo100@gmail.com>
2023-02-05 15:03:33 -0600

    bluetooth update

--- arm_control_flutter/lib/bluetooth.dart
+++ arm_control_flutter/lib/bluetooth.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_blue/flutter_blue.dart';
 import 'package:flutter/material.dart';
-import 'package:wece_glasses/globals.dart';
 
 // Bluetooth connection screen
 class BluetoothConnectScreen extends StatefulWidget {
---
+++ arm_control_flutter/lib/globals.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_blue/flutter_blue.dart';
+import 'package:arm_control_flutter/bluetooth_handler.dart';
+
+//late final BLEHandler bleHandler = BLEHandler();
+late final BLEHandler bleHandler;
